b92c1894dd2e5c0eaefb5de4a9432231876ed5b9
Julien BULOT <jbulot@student.42.fr>
2026-03-03 23:31:51 +0100

    fix(signals): disable echoctl to prevent ^C display

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,11 +6,12 @@
 /*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 23:03:10 by jbulot            #+#    #+#             */
-/*   Updated: 2026/03/03 23:06:07 by jbulot           ###   ########.fr       */
+/*   Updated: 2026/03/03 23:30:22 by jbulot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <termios.h>
 
 void	handle_sigint(int sig)
 {
@@ -22,8 +23,19 @@ void	handle_sigint(int sig)
 	rl_redisplay();
 }
 
+static void	disable_echoctl(void)
+{
+	struct termios	term;
+
+	if (tcgetattr(STDIN_FILENO, &term) == -1)
+		return ;
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
 void	init_signals(void)
 {
+	disable_echoctl();
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, SIG_IGN);
 }
