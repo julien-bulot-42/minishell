f31dd10c2d6d12ed24857f763379d1ea7f353198
Julien BULOT <jbulot@student.42.fr>
2026-03-03 23:42:44 +0100

    feat(lexer): define token structure and token types

---
+++ includes/lexer.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 23:35:06 by jbulot            #+#    #+#             */
+/*   Updated: 2026/03/03 23:40:49 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+# include "minishell.h"
+
+typedef enum e_token_type
+{
+	T_WORD,
+	T_PIPE,
+	T_REDIR_IN,
+	T_REDIR_OUT,
+	T_REDIR_APPEND,
+	T_HEREDOC
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+t_token	*lexer(char *input);
+void	add_token(t_token **list, char *value, t_token_type type);
+void	free_tokens(t_token *list);
+
+#endif
