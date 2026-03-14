a6948229c40b38b515dbb6436f5bf19c99216fcd
Julien BULOT <jbulot@student.42.fr>
2026-03-14 12:18:20 +0100

    feat: add token structure for lexer

---
+++ include/lexer.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 12:11:59 by jbulot            #+#    #+#             */
+/*   Updated: 2026/03/14 12:17:29 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+# include "minishell.h"
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+/* lexer */
+
+t_token	*lexer(char *input);
+
+/* token*/
+
+t_token	*token_new(char *value, t_token_type type);
+void	token_add_back(t_token **list, t_token *new);
+void	token_print(t_token *list);
+
+#endif
