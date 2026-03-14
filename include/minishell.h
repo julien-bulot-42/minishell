11dbd89c751c9988715006a9bd5f15e37dfc1a62
Julien BULOT <jbulot@student.42.fr>
2026-03-14 11:22:09 +0100

    feat: add main project header

---
+++ include/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 11:18:45 by jbulot            #+#    #+#             */
+/*   Updated: 2026/03/14 11:21:07 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
