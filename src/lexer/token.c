b7527186088c65e39cb956dd8a58cf552ee502ec
Julien BULOT <jbulot@student.42.fr>
2026-03-14 12:26:31 +0100

    feat: implement token linked list utilities

---
+++ src/lexer/token.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 12:18:55 by jbulot            #+#    #+#             */
+/*   Updated: 2026/03/14 12:25:00 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+t_token	*token_new(char *value, t_token_type type)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->value = value;
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
+
+void	token_add_back(t_token **list, t_token *new)
+{
+	t_token	*tmp;
+
+	if (!*list)
+	{
+		*list = new;
+		return ;
+	}
+	tmp = *list;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+
+void	token_print(t_token *list)
+{
+	while (list)
+	{
+		printf("TOKEN: %s\n", list->value);
+		list = list->next;
+	}
+}
