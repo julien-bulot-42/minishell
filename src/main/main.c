dddf8117ccf1e8cfe93ad88f130a8d76f08ed2b7
Julien BULOT <jbulot@student.42.fr>
2026-03-14 14:09:32 +0100

    feat: integrate lexer into main loop for token testing

--- src/main/main.c
+++ src/main/main.c
@@ -6,34 +6,26 @@
 /*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 11:22:59 by jbulot            #+#    #+#             */
-/*   Updated: 2026/03/14 11:34:19 by jbulot           ###   ########.fr       */
+/*   Updated: 2026/03/14 14:02:34 by jbulot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static void	process_line(char *line)
-{
-	if (!line || !*line)
-		return ;
-	printf("input: %s\n", line);
-}
+#include "lexer.h"
 
 int	main(void)
 {
 	char	*line;
+	t_token	*tokens;
 
 	while (1)
 	{
 		line = readline("minishell$ ");
 		if (!line)
-		{
-			printf("exit\n");
 			break ;
-		}
 		add_history(line);
-		process_line(line);
+		tokens = lexer(line);
+		token_print(tokens);
 		free(line);
 	}
-	return (0);
 }
